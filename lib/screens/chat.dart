c5040bba436bfe3918609b4924d4aa909a230374
HarshJivani08 <harshjivani140901@gmail.com>
2024-06-12 16:01:44 +0530

    added log out function

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,6 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget{
+class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
@@ -8,11 +9,20 @@ class ChatScreen extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Theme.of(context).colorScheme.primary,
+              )),
+        ],
       ),
       body: const Center(
-         child: Text('Logged in!'),
+        child: Text('Logged in!'),
       ),
     );
   }
-
 }
